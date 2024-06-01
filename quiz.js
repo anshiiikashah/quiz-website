3741679e8de0f3e507a4658ac48df67e89cb72f4
anshiiikashah <anshikashah54@gmail.com>
2024-06-01 19:43:37 +0530

    Final

--- quiz.js
+++ quiz.js
@@ -1,16 +1,16 @@
-function shuffle(array){
-    function choice(array){
-        return array[Math.floor(Math.random()*(array.length-0)+0)];
+function shuffle(array) {
+    function choice(array) {
+        return array[Math.floor(Math.random() * (array.length - 0) + 0)];
     }
     let copy = [];
-    for (each of array){
+    for (each of array) {
         copy.push(each)
     }
     let shuffled = []
-    for (let i = 0; i<array.length;i++){
+    for (let i = 0; i < array.length; i++) {
         let elem = choice(copy);
         shuffled.push(elem)
-        copy.splice(copy.indexOf(elem),1);
+        copy.splice(copy.indexOf(elem), 1);
     }
     return shuffled;
 }
@@ -36,12 +36,12 @@ class Question {
 }
 
 var all = {
-    "India": [new Question("What is the capital of India", "Delhi", ["Moscow", "Delhi", "Kolkata", "Lucknow"], "Easy"), new Question("Who was India ruled by?", "Britain", ["USA", "Britain", "Egypt", "China"], "Easy"), new Question("When did India get independent", "1947", ["1946", "1947", "1948", "1949"],"Easy")],
+    "India": [new Question("What is the capital of India", "Delhi", ["Moscow", "Delhi", "Kolkata", "Lucknow"], "Easy"), new Question("Who was India ruled by?", "Britain", ["USA", "Britain", "Egypt", "China"], "Easy"), new Question("When did India get independent", "1947", ["1946", "1947", "1948", "1949"], "Easy")],
     "Rivers": [new Question("Which is the longest river in the world?", "Nile", ["Amazon", "Nile", "Yangtze", "Mississippi"], "Easy"), new Question("Which is the largest river in the world?", "Amazon", ["Nile", "Yangtze", "Mekong", "Amazon"], "Easy"), new Question("Which of the following rivers is considered the lifeline of China?", "Yantze", ["Brahmaputra", "Yellow", "Mekong", "Yantze"], "Easy")],
     "Mountains": [new Question("Which of the following is the tallest mountain in the world?", "Mount Everest (Asia)", ["Mount Everest (Asia)", "K2 (Asia)", "Denali (North America)", "Mount Kilimanjaro (Africa)"], "Easy"), new Question("What type of mountain is formed by volcanic eruptions, often having a conical peak?", "Volcanic Mountain", ["Fold Mountain", "Block Mountain", "Volcanic Mountain", "Mesa"], "Easy"), new Question("Which mountain is located on the border between France and Italy?", "Mont Blanc", ["Mont Blanc", "Mount Everest", "K2", "Matterhorn"], "Easy")],
     "Biology": [new Question("What is the process by which water moves through a plant, from the roots to the leaves, and is then released into the air as water vapor?", "Transpiration", ["Respiration", "Photosynthesis", "Transpiration", "Evaporation"], "Easy"), new Question("Which part of a cell contains the genetic material?", "Nucleus", ["Mitochondria", "Nucleus", "Cytoplasm", "Cell Wall"], "Easy"), new Question("What is the scientific term for the 'building blocks of life'?", "Cells", ["Molecules", "Tissues", "Cells", "Organisms"], "Easy")],
     "Physics": [new Question("What is the unit of force in the International System of Units?", "Newton", ["Joule", "Newton", "Pascal", "Watt"], "Easy"), new Question("Who is credited with the discovery of the laws of motion?", "Isaac Newton", ["Galileo Galilei", "Albert Einstein", "Isaac Newton", "Marie Curie"], "Easy"), new Question("What is the scientific term for the 'building blocks of matter'?", "Atoms", ["Molecules", "Cells", "Atoms", "Organisms"], "Easy")],
-    "Chemistry": new Question("What is the smallest unit of matter that still retains the properties of an element?", "Atom", ["Molecule", "Compound", "Atom", "Element"], "Easy")
+    "Chemistry": [new Question("What is the smallest unit of matter that still retains the properties of an element?", "Atom", ["Molecule", "Compound", "Atom", "Element"], "Easy")]
 
 };
 
@@ -59,6 +59,9 @@ var qNo;
 var qSet;
 var currQ;
 var quizz;
+var answered = false;
+var maxTime;
+
 function setQuizz(name) {
     difficultyBox.style.display = "flex";
     document.querySelector("#tag").style.display = "none";
@@ -66,14 +69,28 @@ function setQuizz(name) {
     document.querySelectorAll(".div1")[0].style.display = "none";
 
     quizz = name;
-    for (cat of category){
+    for (cat of category) {
         cat.style.display = "none"
     }
     document.querySelector("#quizzName").innerText = quizz;
 }
-function gatherQuestions(quiz, level) {
 
+function setTime(level) {
+    if (level == "Easy") {
+        return 30
+    }
+    else if (level == "Medium") {
+        return 40
+    }
+    else {
+        return 50;
+    }
+}
+
+function gatherQuestions(quiz, level) {
+    maxTime = setTime(level)
     let q = []
+    console.log()
     for (each of all[quiz]) {
         if (each.level == level) {
             q.push(each)
@@ -87,9 +104,10 @@ function startQuiz(level) {
     difficultyBox.style.display = "none"
     qNo = 0;
     qSet = gatherQuestions(quizz, level)
+    setTime(level);
     if (qSet.length == 0) {
-        
-    calculatorView.style.display = "none";
+
+        calculatorView.style.display = "none";
         alert("No questions in this difficulty");
 
         difficultyBox.style.display = "flex"
@@ -98,7 +116,7 @@ function startQuiz(level) {
         for (each of category) {
             each.style.display = "none";
         }
-        
+
         currQ = qSet[qNo]
         showQuestion(currQ)
         document.querySelector("#timer").style.display = "flex";
@@ -121,11 +139,13 @@ function showQuestion(q) {
 }
 
 function checkAnswer(n) {
+    alert(`Elapsed time in this question is ${maxTime - time} second(s)`)
     stopTimer()
+    answered = true;
     let answer = currQ.answer;
     let userAnswer = currQ.options[n]
     currQ.addAnswer(userAnswer)
-    currQ.addTime(30 - time);
+    currQ.addTime(maxTime - time);
     if (userAnswer == answer) {
         document.querySelector(`#option-${n + 1}`).style.backgroundColor = "green"
     }
@@ -137,23 +157,32 @@ function checkAnswer(n) {
 
 function nextQuestion() {
     qNo++;
-    currQ = qSet[qNo]
-    showQuestion(currQ)
-    opt1.style.backgroundColor = "white"
-    opt2.style.backgroundColor = "white"
-    opt3.style.backgroundColor = "white"
-    opt4.style.backgroundColor = "white"
-    timerSpan.innerText = 30;
+    if (qNo == qSet.length) {
+        viewProgress();
+    }
+    else {
+        currQ = qSet[qNo]
+        showQuestion(currQ)
+        opt1.style.backgroundColor = "white"
+        opt2.style.backgroundColor = "white"
+        opt3.style.backgroundColor = "white"
+        opt4.style.backgroundColor = "white"
+        timerSpan.innerText = maxTime;
+    }
 }
 
 var interval;
-var time = 30;
+var time = maxTime;
 
 function startTimer() {
-    time = 30;
+    time = maxTime;
     interval = setInterval(() => {
         time -= 1;
         timerSpan.innerText = time;
+        if (time == 0) {
+            stopTimer();
+            nextQuestion()
+        }
     }, 1000)
 }
 
@@ -161,3 +190,41 @@ function stopTimer() {
     clearInterval(interval)
 }
 
+
+function viewProgress() {
+    document.querySelector("#progressBar").style.display = "flex";
+    document.querySelectorAll(".questionBox")[0].style.display = "none";
+    document.querySelector("#timer").style.display = "none";
+    let totalQuestion = qSet.length;
+    let qCorrect = 0;
+    let qUnattempted = 0;
+    for (each of qSet) {
+        if (each.user == each.answer) {
+            qCorrect++;
+        }
+        if (each.user == "") {
+            qUnattempted++;
+        }
+    }
+    let percentage = (qCorrect / totalQuestion) * 100;
+
+    document.querySelector("#totalQuestionSpan").innerText = totalQuestion;
+    document.querySelector("#correctAnsweredSpan").innerText = qCorrect;
+    document.querySelector("#unattemptedQuestionsSpan").innerText = qUnattempted;
+    document.querySelector("#percentageSpan").innerText = percentage;
+    let comment = ""
+    if (percentage <= 40) {
+        comment = "Try Again"
+    }
+    else if (percentage >= 41 && percentage <= 80) {
+        comment = "Good"
+    }
+    else {
+        comment = "Excellent"
+    }
+    document.querySelector("#commentSpan").innerText = comment;
+}
+
+document.querySelector("#back2Home").addEventListener('click', () => {
+    location.reload()
+})
--- search.js
+++ search.js
@@ -1,4 +1,57 @@
 const dd = document.querySelector("#dropdown");
-dd.addEventListener('click',()=>{
-    
+const quizBox = document.querySelectorAll(".quizBox");
+const allCategories = document.querySelectorAll(".category")
+
+function showOGScreen() {
+    for (each of allCategories) {
+        each.style.display = "none";
+    }
+    for (each of quizBox) {
+        each.style.display = "none";
+    }
+    document.querySelector("#container").style.display = "flex";
+}
+
+dd.addEventListener('click', () => {
+    for (each of allCategories) {
+        each.style.display = "none";
+    }
+    if (dd.value == "") {
+        showOGScreen()
+    }
+    else {
+
+        document.querySelector("#container").style.display = "none";
+        document.querySelector(`#${dd.value}Quizzes`).style.display = "grid";
+        let quizzes = document.querySelectorAll(`#${dd.value}Quizzes>div`);
+        for (each of quizzes) {
+            each.style.display = "flex";
+        }
+    }
 })
+
+
+const searchQuiz = document.querySelector("#searchQuiz");
+
+searchQuiz.addEventListener('input', () => {
+    let elem = document.getElementById(searchQuiz.value)
+    console.log(searchQuiz.value)
+    if (elem == undefined) {
+        showOGScreen()
+    }
+    else {
+        
+        document.querySelector("#container").style.display = "none";
+        for (each of quizBox) {
+            each.style.display = "none";
+        }
+        console.log(elem)
+        elem.style.display = "flex";
+        for (each of allCategories){
+            each.style.display = "grid"
+        }
+
+    }
+})
+
+showOGScreen()
